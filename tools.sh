caac4cce47b203167a855d45ebe401792b58ad43
sterling duncan <Adlaugh@net-shopping.com>
2024-10-17 01:37:44 +0000

    experimenting

--- tools.sh
+++ tools.sh
@@ -1,5 +1,11 @@
 sudo apt install xorriso
 sudo apt install grub-pc
 git clone https://github.com/lordmilko/i686-elf-tools  
-cd i686-elf-tools
- ../i686-elf-tools.sh linux
+ sudo apt install tasm
+ sudo apt install bcc
+ sudo apt install perl
+ sudo apt install python3
+ sudo apt install nasm
+ sudo apt install gas
+ sudo apt install grep 
+ sudo apt install sed
