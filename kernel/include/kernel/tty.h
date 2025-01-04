289decd2a3d14ab3edccc1365a56faa028575d58
InLnx <adlaugh@net-shopping.com>
2025-01-03 23:09:27 -0600

    its all going soon, I have to restart

--- kernel/include/kernel/tty.h
+++ kernel/include/kernel/tty.h
@@ -3,9 +3,9 @@
 
 #include <stddef.h>
 
-void terminal_initialize(void);
+void terminal_initialize();
 void terminal_putchar(char c);
 void terminal_write(const char* data, size_t size);
 void terminal_writestring(const char* data);
 
-#endif
+#endif // _KERNEL_TTY_H
